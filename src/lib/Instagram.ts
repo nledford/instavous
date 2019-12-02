990e1a5f915052af7404c3e1e7f187819370c06c
Nathaniel Ledford <nate@nateledford.com>
2019-12-02 11:20:17 -0500

    Refactor stuff

--- src/lib/Instagram.ts
+++ src/lib/Instagram.ts
@@ -1,12 +1,48 @@
-import { IgApiClient } from 'instagram-private-api'
-import { UserFeedResponseItemsItem } from 'instagram-private-api/dist/responses'
+import fs from 'fs'
+import path from 'path'
+
+import emptyDir from 'empty-dir'
+import { IgApiClient, UserFeed } from 'instagram-private-api'
+import { 
+    UserFeedResponseItemsItem,
+    UserFeedResponseCarouselMediaItem,
+    UserFeedResponseImage_versions2 
+} from 'instagram-private-api/dist/responses'
 import moment from 'moment'
 
 import Files from './Files'
 
+enum InstaMediaType {
+    Image = 1,
+    Video = 2,
+    Carousel = 8,
+}
 
+const getAllItemsFromFeed = async (feed: UserFeed): Promise<UserFeedResponseItemsItem[]> => {
+    let items: UserFeedResponseItemsItem[] = []
+    do {
+        items = items.concat(await feed.items())
+    } while (feed.isMoreAvailable())
+    return items
+}
+
+const toDatedFolderPath = (unixTimestamp: number): string => {
+    const date = moment.unix(unixTimestamp)
+
+    const year = date.format('YYYY')
+    const month = date.format('MM')
+    const day = date.format('DD')
+
+    return path.join(year, month, day)
+}
+
+const downloadMediaAsync = async (media: UserFeedResponseCarouselMediaItem | UserFeedResponseImage_versions2) => {
+
+}
 
 export default class Instagram {
+    public posts: any[] = []
+
     public static async downloadUserMediaAsync(client: IgApiClient, username: string): Promise<void> {
         // Retrieve user information
         console.log(`Retriving info for ${username}...`)
@@ -19,16 +55,44 @@ export default class Instagram {
         // Load media...
         console.log(`Loading media for ${user.username}...`)
 
-        // const allItems: UserFeedResponseItemsItem[] = []
         const feed = client.feed.user(user.pk)
+        const posts = await getAllItemsFromFeed(feed)
+        console.log(`Number of posts to download: ${posts.length}`)
 
+        const post = posts[0]
+        console.log(post)
 
+        const isCarousel = post.carousel_media_count !== undefined
+        console.log(isCarousel)
 
-        const items = await feed.items();
-        console.log(items[0])
+        const datedDir = Files.getDirectory(userDir, toDatedFolderPath(post.taken_at))
+        console.log(datedDir)
+        console.log(post.carousel_media![0].image_versions2[0])
 
-        const post = items[0]
-        const date = moment.unix(post.taken_at).format()
-        console.log(date)
+        if (isCarousel) {
+            const setsDir = Files.getDirectory(datedDir, 'Sets')
+
+            // check if sets directory is empty
+            let setDir: string
+            if (await emptyDir(setsDir)) {
+                setDir = Files.getDirectory(setsDir, '001')
+            } else {
+                const numberOfDirs = fs.readdirSync(setsDir).length
+                setDir = `${numberOfDirs + 1}`.padStart(3, '0')
+                setDir = Files.getDirectory(setsDir, setDir)
+            }
+
+            let carousel = post.carousel_media!
+            carousel.forEach((carouselItem) => {
+                const takenAt = moment.unix(post.taken_at)
+
+                switch(carouselItem.media_type) {
+                    // image
+                    case 1:
+                        const img = carouselItem.image_versions2[0]
+                }
+            }) 
+        }
+
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 require('dotenv').config()
 
+import inquirer from 'inquirer'
 import { IgApiClient } from 'instagram-private-api'
 import moment from 'moment'
 
@@ -8,29 +9,53 @@ import Instagram from './lib/Instagram'
 console.log("Instavous\n")
 
 // set up constants
-const username = process.env.INSTAGRAM_USER!
-const password = process.env.INSTAGRAM_PASSWORD!
+const INSTAGRAM_USER = process.env.INSTAGRAM_USER
+const INSTAGRAM_PASSWORD = process.env.INSTAGRAM_PASSWORD
 
-const account = 'shanimarie_nsb'
+if (INSTAGRAM_USER === undefined || INSTAGRAM_PASSWORD === undefined) {
+    console.log('No username and/or password provided')
+    process.exit(1)
+}
+
+// const account = 'shanimarie_nsb'
+
+const getUsername = async () => {
+    const questions = [
+        {
+            type: 'input',
+            name: 'username',
+            message: 'Enter an Instagram username:'
+        },
+    ]
+
+    const answers = await inquirer.prompt(questions)
+
+    return (answers as any).username
+}
 
 // create instagram client
 const ig = new IgApiClient()
 
+// shanimarie_nsb
+// monicapaulette
+
 // generate device id with username
-ig.state.generateDevice(username);
+ig.state.generateDevice(INSTAGRAM_USER!);
 
 (async () => {
     // Simulate pre-login
     await ig.simulate.preLoginFlow()
 
     // Log in
-    const loggedInUser = await ig.account.login(username, password)
+    const loggedInUser = await ig.account.login(INSTAGRAM_USER!, INSTAGRAM_PASSWORD!)
 
     // Simulate post-login
     process.nextTick(async () => await ig.simulate.postLoginFlow())
 
+    const username = await getUsername()
+
     // begin downloading user photos
-    await Instagram.downloadUserMediaAsync(ig, account)
+    await Instagram.downloadUserMediaAsync(ig, username)
 
     console.log(`\nFinished at ${moment(new Date()).format()}`)
 })()
